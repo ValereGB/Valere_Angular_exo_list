fb7ea75a85638f05b4a2f4fa494fedd17b4f08b6
Valere <valeregbv1@gmail.com>
2021-09-02 17:53:59 +0200

    Projet list Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-angular';
+
+  filter: 'tout' | 'en cours' | 'fini' = 'tout';
+  allItems = [
+    { description: 'Manger', done: true },
+    { description: 'Dormir', done: false },
+    { description: 'Ce laver', done: false },
+    { description: 'Jouer', done: false },
+  ];
+
+
+  get items() {
+    if (this.filter === 'tout') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'fini' ? item.done : !item.done);
+  }
+
+  addItem(description: any) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+
+  }
+
+  remove(item: { description: string; done: boolean; }) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
